5ec61887251ae08cf0044a9d52b77ed69cad7d94
LacertosaEdoardo <117066826+LacertosaEdoardo@users.noreply.github.com>
2023-09-19 13:09:00 +0200

    Inizializzazione dei mottoni e gestione bold e caratteri

--- calcolatore/Form1.Designer.cs
+++ calcolatore/Form1.Designer.cs
@@ -28,21 +28,50 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblRisultato = new System.Windows.Forms.Label();
+            this.panelTop = new System.Windows.Forms.Panel();
             this.SuspendLayout();
+            // 
+            // lblRisultato
+            // 
+            this.lblRisultato.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblRisultato.Font = new System.Drawing.Font("Segoe UI Semibold", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRisultato.Location = new System.Drawing.Point(0, 42);
+            this.lblRisultato.Margin = new System.Windows.Forms.Padding(0);
+            this.lblRisultato.Name = "lblRisultato";
+            this.lblRisultato.Size = new System.Drawing.Size(320, 66);
+            this.lblRisultato.TabIndex = 0;
+            this.lblRisultato.Text = "0";
+            this.lblRisultato.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // panelTop
+            // 
+            this.panelTop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTop.Location = new System.Drawing.Point(0, 0);
+            this.panelTop.Name = "panelTop";
+            this.panelTop.Size = new System.Drawing.Size(320, 42);
+            this.panelTop.TabIndex = 1;
             // 
             // CALCULATOR
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 461);
+            this.ClientSize = new System.Drawing.Size(320, 477);
+            this.Controls.Add(this.lblRisultato);
+            this.Controls.Add(this.panelTop);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
             this.Name = "CALCULATOR";
             this.Text = "CALCULATOR";
+            this.Load += new System.EventHandler(this.CALCULATOR_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblRisultato;
+        private System.Windows.Forms.Panel panelTop;
     }
 }
 
--- calcolatore/Form1.cs
+++ calcolatore/Form1.cs
@@ -12,9 +12,60 @@ namespace calcolatore
 {
     public partial class CALCULATOR : Form
     {
+        public struct btnStruct
+        {
+            public char Content;
+            public bool isBold;
+            public btnStruct(char content, bool isBold)
+            {
+                this.Content = content;
+                this.isBold = isBold;
+            }
+        }
+
+        private btnStruct[,] button =
+        {
+            {new btnStruct('%',false), new btnStruct('C',false), new btnStruct('c',false), new btnStruct('<',false)},
+            {new btnStruct('x',false), new btnStruct('x',false), new btnStruct('c',false), new btnStruct('/',false)},
+            {new btnStruct('7',true), new btnStruct('8',true), new btnStruct('9',true), new btnStruct('x',false)},
+            {new btnStruct('4',true), new btnStruct('5',true), new btnStruct('6',true), new btnStruct('-',false)},
+            {new btnStruct('1',true), new btnStruct('2',true), new btnStruct('3',true), new btnStruct('+',false)},
+            {new btnStruct('|',false), new btnStruct('0',false), new btnStruct(',',false), new btnStruct('=',false)}
+        };
         public CALCULATOR()
         {
             InitializeComponent();
+        }
+
+        private void CALCULATOR_Load(object sender, EventArgs e)
+        {
+            GenerateButton();
+        }
+
+        private void GenerateButton()
+        {
+            int btnWidth = 80;
+            int btnHeight = 60;
+            int posX = 0;
+            int posY = 117;
+
+            for(int i=0;i<6; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    Button myButton = new Button();
+                    myButton.Font = new Font("Segoe UI", 16);
+                    myButton.Text = button[i,j].Content.ToString();
+                    myButton.Height = btnHeight;
+                    myButton.Width = btnWidth;
+                    myButton.Top = posY;
+                    myButton.Left = posX;
+                    posX += myButton.Width;
+                    this.Controls.Add(myButton);
+                }
+                posX = 0;
+                posY += 60;
+            }
         }
     }
 }
